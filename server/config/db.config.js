626fabdb8eab1fe0957bbd5ca3f01a61fd49baa1
Arpit Raj <130226536+arpit-raj31@users.noreply.github.com>
2025-05-07 12:33:14 +0530

    Update db.config.js

--- server/config/db.config.js
+++ server/config/db.config.js
@@ -1,14 +1,13 @@
-// db.config.js
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password: '', // <-- Add your password here
+  password: '', // <-- your MySQL password
   database: 'tradecfd1',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
 });
 
-module.exports = pool.promise();
+module.exports = pool;
