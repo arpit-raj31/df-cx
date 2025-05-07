ad7d6c7fba2f51a1788ce7c408670ada1428dec4
Arpit Raj <130226536+arpit-raj31@users.noreply.github.com>
2025-05-07 12:33:59 +0530

    Update queries.js

--- server/queries/queries.js
+++ server/queries/queries.js
@@ -1,5 +1,5 @@
+const insertUser = `INSERT INTO users (firstName, userName, email, mobile, password) VALUES (?, ?, ?, ?, ?)`;
 
-exports.insertUser = `
-  INSERT INTO users (firstName, userName, email, mobile, password, created_at, updated_at)
-  VALUES (?, ?, ?, ?, ?, NOW(), NOW())
-`;
+module.exports = {
+  insertUser,
+};
